37ae917464c0dda50615fc0efdac75474f9ea2b3
Rodion Terekhin <rodionterekhin@gmail.com>
2025-03-01 22:10:01 +0300

    wip: add core functionality

---
+++ github_api/__init__.py
@@ -0,0 +1,3 @@
+from .exceptions import GithubAPIError
+from .models import Event, Events, EventType, RefType
+from .user import get_user_events
---
+++ github_api/consts.py
@@ -0,0 +1,2 @@
+BASE_URL = "https://api.github.com"
+USER_EVENTS = "/users/{0}/events"
