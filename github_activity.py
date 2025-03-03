1d8f80685c7a3b1c3ae0df7902e3b2ad9406f410
Rodion Terekhin <rodionterekhin@gmail.com>
2025-03-03 19:25:36 +0300

    minor text formatting changes

--- github_activity.py
+++ github_activity.py
@@ -22,18 +22,18 @@ def get_args_or_fail() -> argparse.Namespace:
 
 
 def print_event(event: Event) -> None:
-    txt: str = f"{event.created_at}: "
+    txt: str = f"{event.created_at:%Y-%m-%d %H:%M:%S}: "
     match event.type:
         case EventType.COMMIT_COMMENT_EVENT:
             txt += "Created a comment on commit"
         case EventType.CREATE_EVENT:
             txt += f"Created a {event.payload.ref_type.value}"
             if event.payload.ref:
-                txt += f" (ref = {event.payload.ref}"
+                txt += f' "{event.payload.ref}"'
         case EventType.DELETE_EVENT:
             txt += f"Deleted a {event.payload.ref_type.value}"
             if event.payload.ref:
-                txt += f" (ref = {event.payload.ref})"
+                txt += f' "{event.payload.ref}"'
         case EventType.FORK_EVENT:
             txt += f"Forked a repo {event.payload.forkee.full_name}"
         case EventType.GOLLUM_EVENT:
@@ -56,9 +56,17 @@ def print_event(event: Event) -> None:
                 " thread"
             )
         case EventType.PUSH_EVENT:
-            txt += (
-                f"{event.payload.size} commits pushed to {event.payload.ref}"
-            )
+            if event.payload.size == 0:
+                if event.payload.distinct_size == 0:
+                    txt += "One commit"
+                else:
+                    txt += f"{event.payload.distinct_size} commits"
+                txt += " swapped in"
+            elif event.payload.size == 1:
+                txt += "One commit pushed to"
+            else:
+                txt += f"{event.payload.size} commmits pushed to"
+            txt += f' "{event.payload.ref}"'
         case EventType.RELEASE_EVENT:
             txt += f"{event.payload.action} a release"
         case EventType.SPONSORSHIP_EVENT:
